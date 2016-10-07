18a0ec75319904aea4e18f2097ca2aade43000a0
Fabien Potencier <fabien.potencier@gmail.com>
2016-10-07 15:35:28 -0700

    -

--- SymfonyStartPlugin.php
+++ SymfonyStartPlugin.php
@@ -21,33 +21,94 @@ class SymfonyStartPlugin implements PluginInterface, EventSubscriberInterface
     {
         $package = $event->getOperation()->getPackage();
 
-        $dir = __DIR__.'/../../fabpot/symfony-start/config-repo/'.$package->getName();
+        $dir = __DIR__.'/../symfony-start/config-repo/'.$package->getName();
         if (!is_dir($dir)) {
             return;
         }
 
-        $this->io->write(sprintf('Auto-configuring "%s" as a Symfony bundle', $package->getName()));
-
-        print getcwd()."***\n";
+        $this->io->write(sprintf('    Auto-configuring "%s" as a Symfony bundle', $package->getName()));
+        $this->addBundle($package, $dir);
+        $this->addBundleConfig($package, $dir);
     }
 
     public function updateConfig(PackageEvent $event)
     {
-        
     }
 
     public function removeConfig(PackageEvent $event)
     {
         $package = $event->getOperation()->getPackage();
 
-        $dir = __DIR__.'/../../fabpot/symfony-start/config-repo/'.$package->getName();
+        $dir = __DIR__.'/../symfony-start/config-repo/'.$package->getName();
         if (!is_dir($dir)) {
             return;
         }
 
-        $this->io->write(sprintf('Auto-deconfiguring "%s" as a Symfony bundle', $package->getName()));
+        $this->io->write(sprintf('    Auto-deconfiguring "%s" as a Symfony bundle', $package->getName()));
+        $this->removeBundle($package, $dir);
+        $this->removeBundleConfig($package, $dir);
+    }
 
-        print getcwd()."***\n";
+    private function addBundle($package, $dir)
+    {
+        $this->io->write(sprintf('      - Adding "%s" from bundles.ini', $package->getName()));
+        $bundlesini = getcwd().'/conf/bundles.ini';
+        $contents = file_get_contents($bundlesini);
+        foreach ($this->parseBundles($dir) as $class => $envs) {
+            $contents .= "$class = $envs\n";
+        }
+        file_put_contents($bundlesini, $contents);
+    }
+
+    private function addBundleConfig($package, $dir)
+    {
+        $this->io->write(sprintf('      - Configuring "%s"', $package->getName()));
+// FIXME: make this conf/ directory configurable via composer.json
+        $target = getcwd().'/conf';
+        $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir.'/conf', \RecursiveDirectoryIterator::SKIP_DOTS), \RecursiveIteratorIterator::SELF_FIRST);
+        foreach ($iterator as $item) {
+            if ($item->isDir()) {
+                if (!is_dir($new = $target.'/'.$iterator->getSubPathName())) {
+                    mkdir($new);
+                }
+            } else {
+                copy($item, $target.'/'.$iterator->getSubPathName());
+            }
+        }
+    }
+
+    private function removeBundle($package, $dir)
+    {
+        $this->io->write(sprintf('      - Removing "%s" from bundles.ini', $package->getName()));
+        $bundlesini = getcwd().'/conf/bundles.ini';
+        $contents = file_get_contents($bundlesini);
+        foreach (array_keys($this->parseBundles($dir)) as $class) {
+            $contents = preg_replace('/^'.preg_quote($class, '/').'.+$/m', '', $contents);
+            $contents = preg_replace("/\n+/", "\n", $contents);
+        }
+        file_put_contents($bundlesini, $contents);
+    }
+
+    private function removeBundleConfig($package, $dir)
+    {
+        $this->io->write(sprintf('      - Removing configuration for "%s"', $package->getName()));
+        $target = getcwd().'/conf';
+        $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir.'/conf', \RecursiveDirectoryIterator::SKIP_DOTS), \RecursiveIteratorIterator::SELF_FIRST);
+        foreach ($iterator as $item) {
+            if (!$item->isDir()) {
+                @unlink($target.'/'.$iterator->getSubPathName());
+            }
+        }
+    }
+
+    private function parseBundles($dir)
+    {
+        $bundles = [];
+        foreach (parse_ini_file($dir.'/bundles.ini') as $class => $envs) {
+            $bundles[$class] = $envs;
+        }
+
+        return $bundles;
     }
 
     public static function getSubscribedEvents()
