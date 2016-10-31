f9a29b630d1629325614e63fce3a9eda9fff7798
Fabien Potencier <fabien.potencier@gmail.com>
2016-10-30 20:58:58 -0700

    -

---
+++ PackageRequirement.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Symfony\Start;
+
+class PackageRequirement
+{
+    private $package;
+    private $constraint;
+    private $dev;
+
+    public function __construct($package, $constraint, $dev)
+    {
+        $this->package = $package;
+        $this->constraint = $constraint;
+        $this->dev = (bool) $dev;
+
+        $versionParser = new VersionParser();
+        $versionParser->parseConstraints($constraint);
+    }
+
+    public function getPackage()
+    {
+        return $this->package;
+    }
+
+    public function getConstraint()
+    {
+        return $this->constraint;
+    }
+
+    public function isDev()
+    {
+        return $this->dev;
+    }
+
+    public function getRemoveKey()
+    {
+        return $this->dev ? 'require' : 'require-dev';
+    }
+
+    public function getRequireKey()
+    {
+        return $this->dev ? 'require-dev' : 'require';
+    }
+}
