03ade0b6d9158ae0b247f844ec196d0121fd34c7
Fabien Potencier <fabien.potencier@gmail.com>
2016-12-06 11:21:06 +0100

    -

--- src/Configurator/ParametersConfigurator.php
+++ src/Configurator/ParametersConfigurator.php
@@ -14,38 +14,25 @@ class ParametersConfigurator extends AbstractConfigurator
 
     public function unconfigure(Recipe $recipe, $parameters)
     {
-// FIXME: what about parameters.ini, difficult to revert that (too many possible side effect
+// FIXME: what about parameters.yaml, difficult to revert that (too many possible side effect
 //        between bundles changing the same value)
     }
 
     private function updateParametersIni($parameters)
     {
-        $target = getcwd().'/conf/parameters.ini';
-        $original = $this->readIniRaw($target);
-        $contents = rtrim(file_get_contents($target), "\n")."\n";
+        $target = getcwd().'/conf/parameters.yaml';
+        $contents = file_get_contents($target);
         foreach ($parameters as $key => $value) {
-            if (isset($original['parameters'][$key])) {
-                $value = var_export($value, true);
-                // replace value
-                $contents = preg_replace('{^( *)'.$key.'( *)=( *).*$}im', "$1$key$2=$3$value", $contents);
-            } else {
-                // add a new entry
-                $contents .= "  $key = $value\n";
+// FIXME: we don't have access to YAML here :( Or can we?
+// so, only string, bools, int... work here
+            $value = var_export($value, true);
+            $count = 0;
+            $contents = preg_replace('{^( *)'.$key.'( *):( *).*$}im', "$1$key$2:$3$value", $contents, -1, $count);
+            if (!$count) {
+                $contents .= "    $key: $value\n";
             }
         }
 
         file_put_contents($target, $contents);
-    }
-
-    private function readIniRaw($file)
-    {
-        // first pass to catch parsing errors
-        $result = parse_ini_file($file, true);
-        if (false === $result || array() === $result) {
-            throw new InvalidArgumentException(sprintf('The "%s" file is not valid.', $file));
-        }
-
-        // real raw parsing
-        return parse_ini_file($file, true, INI_SCANNER_RAW);
     }
 }
