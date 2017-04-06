b8bcfaf1002d538cca383a78bacc0428d0311c0f
Fabien Potencier <fabien.potencier@gmail.com>
2017-04-06 15:51:04 -0700

    fixed error message when an alias cannot be resolved to a package

--- src/PackageResolver.php
+++ src/PackageResolver.php
@@ -30,6 +30,8 @@ class PackageResolver
 
     public function resolve(array $arguments = [])
     {
+        $versionParser = new VersionParser();
+
         // first pass split on : and = to separate package names and versions
         $explodedArguments = [];
         foreach ($arguments as $argument) {
@@ -49,8 +51,15 @@ class PackageResolver
                     self::$aliases = $this->downloader->getContents('/aliases.json');
                 }
 
-                while (isset(self::$aliases[$argument])) {
+                if (isset(self::$aliases[$argument])) {
                     $argument = self::$aliases[$argument];
+                } else {
+                    // is it a version or an alias that does not exist?
+                    try {
+                        $versionParser->parseConstraints($argument);
+                    } catch (\UnexpectedValueException $e) {
+                        $this->throwAlternatives($argument);
+                    }
                 }
             }
 
@@ -59,7 +68,7 @@ class PackageResolver
 
         // third pass to resolve versions
         $requires= [];
-        foreach ((new VersionParser())->parseNameVersionPairs($packages) as $package) {
+        foreach ($versionParser->parseNameVersionPairs($packages) as $package) {
             $requires[] = $package['name'].$this->parseVersion($package['name'], $package['version'] ?? null);
         }
 
@@ -87,5 +96,32 @@ class PackageResolver
         }
 
         return ':'.$version;
+    }
+
+    private function throwAlternatives($argument)
+    {
+        $alternatives = [];
+        foreach (self::$aliases as $alias => $package) {
+            $lev = levenshtein($argument, $alias);
+            if ($lev <= strlen($argument) / 3 || false !== strpos($alias, $argument)) {
+                $alternatives[$package][] = $alias;
+            }
+        }
+
+        if ($alternatives) {
+            $message = sprintf('"%s" is not a valid alias. ', $argument);
+            if (1 == count($alternatives)) {
+                $message .= "Did you mean this:\n";
+            } else {
+                $message .= "Did you mean one of these:\n";
+            }
+            foreach ($alternatives as $package => $aliases) {
+                $message .= sprintf("  \"%s\", supported aliases: \"%s\"\n", $package, implode('", "', $aliases));
+            }
+        } else {
+            $message = sprintf('Could not parse version constraint "%s".', $argument);
+        }
+
+        throw new \UnexpectedValueException($message);
     }
 }
