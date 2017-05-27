473af98c7cfda5c98e7c8dc5984c28a8a513bea5
Fabien Potencier <fabien.potencier@gmail.com>
2017-05-27 08:26:36 -0700

    fixed container configurator

--- src/Configurator/ContainerConfigurator.php
+++ src/Configurator/ContainerConfigurator.php
@@ -21,28 +21,38 @@ class ContainerConfigurator extends AbstractConfigurator
     public function configure(Recipe $recipe, $parameters): void
     {
         $this->io->write('    Setting parameters');
-        $this->updateParametersIni($parameters);
+        $this->addParameters($parameters);
     }
 
     public function unconfigure(Recipe $recipe, $parameters): void
     {
-        // FIXME: hard to do, but as adding parameters should be very rare, that's fine
+        $target = getcwd().'/etc/container.yaml';
+        $lines = [];
+        foreach (file($target) as $line) {
+            foreach (array_keys($parameters) as $key) {
+                if (preg_match("/^\s+$key\:/", $line)) {
+                    continue 2;
+                }
+            }
+            $lines[] = $line;
+        }
+        file_put_contents($target, implode('', $lines));
     }
 
-    private function updateParametersIni(iterable $parameters): void
+    private function addParameters(iterable $parameters): void
     {
         $target = getcwd().'/etc/container.yaml';
-        $contents = file_get_contents($target);
-        foreach ($parameters as $key => $value) {
-            // FIXME: var_export() only works for basics types, but we don't have access to the Symfony YAML component here
-            $value = var_export($value, true);
-            $count = 0;
-            $contents = preg_replace('{^( *)'.$key.'( *):( *).*$}im', "$1$key$2:$3$value", $contents, -1, $count);
-            if (!$count) {
-                $contents .= "    $key: $value".PHP_EOL;
+        $lines = [];
+        foreach (file($target) as $line) {
+            $lines[] = $line;
+            if (!preg_match('/^parameters\:/', $line)) {
+                continue;
+            }
+            foreach ($parameters as $key => $value) {
+                // FIXME: var_export() only works for basics types, but we don't have access to the Symfony YAML component here
+                $lines[] = sprintf("    %s: %s%s", $key, var_export($value, true), PHP_EOL);
             }
         }
-
-        file_put_contents($target, $contents);
+        file_put_contents($target, implode('', $lines));
     }
 }
---
+++ tests/Configurator/TmpDirMock.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Symfony\Flex\Configurator;
+
+function getcwd()
+{
+    return sys_get_temp_dir();
+}
