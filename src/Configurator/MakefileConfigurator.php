4b2e0e0d0a9e1131bb9b4931af2fdc9aa8942f8f
Jonathan <Grafikart@users.noreply.github.com>
2017-09-15 19:17:02 +0200

    Fix spacing

--- src/Configurator/MakefileConfigurator.php
+++ src/Configurator/MakefileConfigurator.php
@@ -36,7 +36,7 @@ ifndef APP_ENV
 endif
 
 .DEFAULT_GOAL := help
-.PHONY : help
+.PHONY: help
 help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' Makefile | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}'
 
