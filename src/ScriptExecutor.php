8ee5fcd822fc360b61e86d91c69196b1aca69fcb
Chase Noel <chase.i.noel@gmail.com>
2017-10-18 13:44:33 -0600

    Removed spaces

--- src/ScriptExecutor.php
+++ src/ScriptExecutor.php
@@ -129,6 +129,6 @@ class ScriptExecutor
 
         $phpArgs = implode(' ', array_map('escapeshellarg', $arguments));
 
-        return escapeshellarg($php) . ($phpArgs ? ' '.$phpArgs : '') . ' ' . $cmd;
+        return escapeshellarg($php).($phpArgs ? ' '.$phpArgs : '').' '.$cmd;
     }
 }
