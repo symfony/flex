308bccfbabb1b03810a01c97331458900102bb28
Andreas Möller <am@localheinz.com>
2018-04-28 00:02:40 +0200

    Fix: Do not declare multiple classes in one file

---
+++ src/Command/EmptyEvent.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Symfony\Flex\Command;
+
+use Composer\Script\Event;
+
+class EmptyEvent extends Event
+{
+    public function __construct()
+    {
+    }
+}
--- src/Command/FixRecipesCommand.php
+++ src/Command/FixRecipesCommand.php
@@ -14,7 +14,6 @@ namespace Symfony\Flex\Command;
 use Composer\Command\BaseCommand;
 use Composer\DependencyResolver\Operation\InstallOperation;
 use Composer\Factory;
-use Composer\Script\Event;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Flex\Lock;
@@ -76,12 +75,5 @@ class FixRecipesCommand extends BaseCommand
         }
 
         $this->flex->update(new EmptyEvent(), $operations);
-    }
-}
-
-class EmptyEvent extends Event
-{
-    public function __construct()
-    {
     }
 }
