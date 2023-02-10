9aca8f380b0191f9555edfb39863d1b14aef771b
Nicolas Grekas <nicolas.grekas@gmail.com>
2023-02-10 08:34:20 +0100

    Fix keeping platform requirements when rebooting composer

--- src/Flex.php
+++ src/Flex.php
@@ -1033,6 +1033,9 @@ class Flex implements PluginInterface, EventSubscriberInterface
             $composer->getEventDispatcher(),
             $composer->getAutoloadGenerator()
         );
+        if (method_exists($installer, 'setPlatformRequirementFilter')) {
+            $installer->setPlatformRequirementFilter(((array) $this->installer)["\0*\0platformRequirementFilter"]);
+        }
 
         if (!$update && method_exists($installer, 'setUpdateAllowList')) {
             $installer->setUpdateAllowList(['php']);
