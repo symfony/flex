42276be3bf961444110e6856c14cc9f047b65d05
Nicolas Grekas <nicolas.grekas@gmail.com>
2023-10-30 19:21:35 +0100

    Decouple psr-http-message-bridge from extra.symfony.require for versions before 6.4

--- src/PackageFilter.php
+++ src/PackageFilter.php
@@ -80,6 +80,7 @@ class PackageFilter
                 !isset($knownVersions['splits'][$name])
                 || array_intersect($versions, $lockedVersions[$name] ?? [])
                 || (isset($rootConstraints[$name]) && !Intervals::haveIntersections($this->symfonyConstraints, $rootConstraints[$name]))
+                || ('symfony/psr-http-message-bridge' === $name && 6.4 > $versions[0])
             )) {
                 $filteredPackages[] = $package;
                 continue;
