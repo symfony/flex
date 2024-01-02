77e7ba3feac0e0589404c91231567cc8e1e80699
Soner Sayakci <s.sayakci@shopware.com>
2024-01-02 11:59:42 +0100

    fix compatibility with Symfony 7

--- src/Command/UnpackCommand.php
+++ src/Command/UnpackCommand.php
@@ -49,7 +49,7 @@ class UnpackCommand extends BaseCommand
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $composer = $this->getComposer();
         $packages = $this->resolver->resolve($input->getArgument('packages'), true);
