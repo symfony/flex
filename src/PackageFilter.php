73537e77148f8b4cac9e1c159af02ea07f4827c9
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-10-30 13:52:52 +0100

    Fix ignoring unstable releases with --prefer-lowest and --prefer-stable

--- src/PackageFilter.php
+++ src/PackageFilter.php
@@ -30,14 +30,16 @@ class PackageFilter
     private $symfonyConstraints;
     private $downloader;
     private $io;
+    private $ignoreUnstableReleases;
 
-    public function __construct(IOInterface $io, string $symfonyRequire, Downloader $downloader)
+    public function __construct(IOInterface $io, string $symfonyRequire, Downloader $downloader, bool $ignoreUnstableReleases = false)
     {
         $this->versionParser = new VersionParser();
         $this->symfonyRequire = $symfonyRequire;
-        $this->symfonyConstraints = $this->versionParser->parseConstraints($symfonyRequire);
+        $this->symfonyConstraints = '' !== $symfonyRequire ? $this->versionParser->parseConstraints($symfonyRequire) : null;
         $this->downloader = $downloader;
         $this->io = $io;
+        $this->ignoreUnstableReleases = $ignoreUnstableReleases;
     }
 
     /**
@@ -48,6 +50,16 @@ class PackageFilter
      */
     public function removeLegacyPackages(array $data, RootPackageInterface $rootPackage, array $lockedPackages): array
     {
+        if ($this->ignoreUnstableReleases) {
+            $filteredPackages = [];
+            foreach ($data as $package) {
+                if (\in_array($package->getStability(), ['stable', 'dev'], true)) {
+                    $filteredPackages[] = $package;
+                }
+            }
+            $data = $filteredPackages;
+        }
+
         if (!$this->symfonyConstraints || !$data) {
             return $data;
         }
